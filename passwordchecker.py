d0eb5adfb729b0761021f28c49e327dfe0a335a3
sourav <souravtolawani04@gmail.com>
2026-02-25 22:55:22 +0530

    adding new project using python

---
+++ passwordchecker.py
@@ -0,0 +1,20 @@
+print("Hello welcome to password checker")
+print("_________________________________")
+
+print("")
+
+number = input("Enter the password")
+
+num_len = len(number)
+
+isNumber = False
+
+for char in number:
+    if char.isdigit():
+        isNumber = True
+
+if (num_len >=8 and isNumber == True):
+    print("Strong Password")
+else:
+    print("Weak Password")
+
